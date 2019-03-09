9863317e5f5eb2f3e7c2b3291e3f40602dce2d12
Conner Simmons <csimmons@mail.thebluebook.com>
2019-03-09 00:05:47 -0500

    updated ios to reset status to none after ended

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -8,44 +8,45 @@ import CallKit
  */
 @objc(CallObserver)
 public class CallObserver: CAPPlugin {
-    
+
     var callStatus = String()
-    
+
     public override func load() {
         let callObserver = CallObserverSingleton.sharedInstance
         callObserver.setDelegate(self, queue: nil)
     }
-    
+
 //    @objc func echo(_ call: CAPPluginCall) {
 //        let value = call.getString("value") ?? ""
 //        call.success([
 //            "value": value
 //        ])
 //    }
-    
-    @objc func observe(_ call: CAPPluginCall) {        
+
+    @objc func observe(_ call: CAPPluginCall) {
         call.success(["data": [
             "status": (self.callStatus == "") ? CallStatus.none.rawValue : self.callStatus
             ]
         ])
+        self.callStatus = (self.callStatus == CallStatus.ended.rawValue) ? CallStatus.none.rawValue : self.callStatus
     }
 }
 
 extension CallObserver: CXCallObserverDelegate {
     public func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
 //        print("CALL: \(call.description)")
-        
+
         if !call.isOutgoing && !call.hasConnected && !call.hasEnded {
             //  Call is incoming
             print("CXCallObserverDelegate: Call is incoming.")
             self.callStatus = CallStatus.incoming.rawValue
         }
-        
+
         if call.isOutgoing && !call.hasConnected {
             //  Dialing out
             print("CXCallObserverDelegate: Call is dialing.")
             self.callStatus = CallStatus.dialing.rawValue
-            
+
             switch UIApplication.shared.applicationState {
             case .active:
                 print("APP STATE: Active")
@@ -58,28 +59,28 @@ extension CallObserver: CXCallObserverDelegate {
                 break
             }
         }
-        
+
         if call.hasConnected && !call.hasEnded {
             //  Call is ongoing
             print("CXCallObserverDelegate: Call is ongoing.")
             self.callStatus = CallStatus.ongoing.rawValue
         }
-        
+
         if call.hasEnded {
             //  Call ended
             print("CXCallObserverDelegate: Call has ended.")
             self.callStatus = CallStatus.ended.rawValue
         }
-        
+
         print("CALL STATUS: \(self.callStatus)")
     }
 }
 
 public class CallObserverSingleton {
-    
+
     static let sharedInstance = CXCallObserver()
     private init() {} //This prevents others from using the default '()' initializer for this class.
-    
+
 }
 
 enum CallStatus: String {
